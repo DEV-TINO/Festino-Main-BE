ccc9281a99b4d86bc0a930fef4d31fc5bd89ff26
rvbear <rvbear2000@gmail.com>
2024-06-25 22:36:12 +0900

    feat: create reservation controller

---
+++ src/main/java/com/DevTino/festino_main/reservation/controller/ReservationController.java
@@ -0,0 +1,10 @@
+package com.DevTino.festino_main.reservation.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class ReservationController {
+
+}
