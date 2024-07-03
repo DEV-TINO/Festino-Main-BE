842ffb44c653af5b45dea32ed15dd244e1cb5155
taegeon <g7193g@naver.com>
2024-07-03 22:37:15 +0900

    [rename] update day, night, food jpa repository name

--- src/main/java/com/DevTino/festino_main/repository/FoodBoothRepositoryJPA.java
+++ src/main/java/com/DevTino/festino_main/repository/FoodBoothRepositoryJPA.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface JPAFoodBoothRepository extends JpaRepository<FoodBoothDAO, UUID> {
+public interface FoodBoothRepositoryJPA extends JpaRepository<FoodBoothDAO, UUID> {
 }
