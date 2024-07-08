6f12249e070add89d6d449d762d47ba6ffe793df
taegeon <g7193g@naver.com>
2024-07-08 17:22:38 +0900

    [fix] main notice add annotation PathVariable

--- src/main/java/com/DevTino/festino_main/notice/controller/NoticeController.java
+++ src/main/java/com/DevTino/festino_main/notice/controller/NoticeController.java
@@ -5,10 +5,7 @@ import com.DevTino.festino_main.notice.service.NoticeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,7 +24,7 @@ public class NoticeController {
     }
 
     @GetMapping("/{noticeId}")
-    public ResponseEntity<Map<String, Object>> getNotice(UUID noticeId){
+    public ResponseEntity<Map<String, Object>> getNotice(@PathVariable UUID noticeId){
         ResponseNoticeGetDTO responseNoticeGetDTO = noticeService.getNotice(noticeId);
 
         boolean success = (responseNoticeGetDTO == null) ? false : true;
