55b14cccd5791b78fb874bf6cf713051b7583029
rvbear <rvbear2000@gmail.com>
2024-07-14 23:00:47 +0900

    remove: RequestOrderGetDTO, RequestReservationGetDTO

--- src/main/java/com/DevTino/festino_main/order/domain/DTO/RequestOrderGetDTO.java
+++
@@ -1,9 +0,0 @@
-package com.DevTino.festino_main.order.domain.DTO;
-
-import lombok.Data;
-
-@Data
-public class RequestOrderGetDTO {
-    String userName;
-    String phoneNum;
-}
--- src/main/java/com/DevTino/festino_main/reservation/domain/DTO/RequestReservationGetDTO.java
+++
@@ -1,9 +0,0 @@
-package com.DevTino.festino_main.reservation.domain.DTO;
-
-import lombok.Data;
-
-@Data
-public class RequestReservationGetDTO {
-    String userName;
-    String phoneNum;
-}
