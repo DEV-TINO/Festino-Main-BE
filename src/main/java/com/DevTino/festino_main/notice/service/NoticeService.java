436a69e71e0981c4ce2a45ed7a680578bbe36c0e
taegeon <g7193g@naver.com>
2024-07-14 21:11:25 +0900

    [refactor] refactoring notice api

--- src/main/java/com/DevTino/festino_main/notice/service/NoticeService.java
+++ src/main/java/com/DevTino/festino_main/notice/service/NoticeService.java
@@ -1,28 +1,38 @@
 package com.DevTino.festino_main.notice.service;
 
 import com.DevTino.festino_main.notice.bean.GetNoticeBean;
+import com.DevTino.festino_main.notice.bean.GetNoticesBean;
 import com.DevTino.festino_main.notice.bean.GetRecentNoticeBean;
 import com.DevTino.festino_main.notice.domain.DTO.ResponseNoticeGetDTO;
+import com.DevTino.festino_main.notice.domain.DTO.ResponseNoticesGetDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
 public class NoticeService {
 
     GetNoticeBean getNoticeBean;
+    GetNoticesBean getNoticesBean;
     GetRecentNoticeBean getRecentNoticeBean;
 
     @Autowired
-    public NoticeService(GetNoticeBean getNoticeBean, GetRecentNoticeBean getRecentNoticeBean){
+    public NoticeService(GetNoticeBean getNoticeBean, GetNoticesBean getNoticesBean, GetRecentNoticeBean getRecentNoticeBean){
         this.getNoticeBean = getNoticeBean;
+        this.getNoticesBean = getNoticesBean;
         this.getRecentNoticeBean = getRecentNoticeBean;
     }
 
     // 특정 공지 조회
     public ResponseNoticeGetDTO getNotice(UUID noticeId){
         return getNoticeBean.exec(noticeId);
+    }
+
+    // 공지 전체 조회
+    public List<ResponseNoticesGetDTO> getNotices(){
+        return getNoticesBean.exec();
     }
 
     // 최근 공지 1개 조회 - pin
