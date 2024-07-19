a2840f56e945b2341a2f94f4e2725187cfebe6bb
rvbear <rvbear2000@gmail.com>
2024-07-20 01:14:07 +0900

    fix: add logic to check booth exist

--- src/main/java/com/DevTino/festino_main/order/bean/SaveOrderBean.java
+++ src/main/java/com/DevTino/festino_main/order/bean/SaveOrderBean.java
@@ -1,5 +1,6 @@
 package com.DevTino.festino_main.order.bean;
 
+import com.DevTino.festino_main.booth.bean.small.GetNightBoothDAOBean;
 import com.DevTino.festino_main.order.bean.small.CreateOrderDAOBean;
 import com.DevTino.festino_main.order.bean.small.SaveOrderDAOBean;
 import com.DevTino.festino_main.order.domain.DTO.RequestOrderSaveDTO;
@@ -13,15 +14,21 @@ import java.util.UUID;
 public class SaveOrderBean {
     CreateOrderDAOBean createOrderDAOBean;
     SaveOrderDAOBean saveOrderDAOBean;
+    GetNightBoothDAOBean getNightBoothDAOBean;
 
     @Autowired
-    public SaveOrderBean(CreateOrderDAOBean createOrderDAOBean, SaveOrderDAOBean saveOrderDAOBean) {
+    public SaveOrderBean(CreateOrderDAOBean createOrderDAOBean, SaveOrderDAOBean saveOrderDAOBean, GetNightBoothDAOBean getNightBoothDAOBean) {
         this.createOrderDAOBean = createOrderDAOBean;
         this.saveOrderDAOBean = saveOrderDAOBean;
+        this.getNightBoothDAOBean = getNightBoothDAOBean;
     }
 
     // 주문 등록
     public UUID exec(RequestOrderSaveDTO requestOrderSaveDTO) {
+        if(getNightBoothDAOBean.exec(requestOrderSaveDTO.getBoothId()) == null) {
+            return null;
+        }
+
         OrderDAO orderDAO = createOrderDAOBean.exec(requestOrderSaveDTO);
         saveOrderDAOBean.exec(orderDAO);
 
