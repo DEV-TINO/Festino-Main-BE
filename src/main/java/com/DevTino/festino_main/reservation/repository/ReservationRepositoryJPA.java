8bbf31a7027621befbb4f67d01eae4f3b028d635
Lee <lsmin476@naver.com>
2024-07-26 22:37:44 +0900

    style: Delete unused methods

--- src/main/java/com/DevTino/festino_main/reservation/repository/ReservationRepositoryJPA.java
+++ src/main/java/com/DevTino/festino_main/reservation/repository/ReservationRepositoryJPA.java
@@ -4,16 +4,11 @@ import com.DevTino.festino_main.reservation.domain.ReservationDAO;
 import com.DevTino.festino_main.reservation.domain.ReservationEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 public interface ReservationRepositoryJPA extends JpaRepository<ReservationDAO, UUID> {
     ReservationDAO findByPhoneNumAndReservationType(String phoneNum, ReservationEnum reservationType);
-
-    List<ReservationDAO> findAllByBoothIdAndReservationTypeAndCreateAtLessThan(UUID boothId, ReservationEnum reservationType, LocalDateTime createAt);
-
-    ReservationDAO findFirstByDateOrderByReservationNumDesc(Integer date);
 
     List<ReservationDAO> findAllByBoothIdOrderByReservationNumAsc(UUID boothId);
 }
