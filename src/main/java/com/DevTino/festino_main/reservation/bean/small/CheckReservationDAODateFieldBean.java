6bcb7d4affe0ee73427dfdc9f15af10c9b3282f2
Lee <lsmin476@naver.com>
2024-09-11 16:22:16 +0900

    fix: time error

--- src/main/java/com/DevTino/festino_main/reservation/bean/small/CheckReservationDAODateFieldBean.java
+++ src/main/java/com/DevTino/festino_main/reservation/bean/small/CheckReservationDAODateFieldBean.java
@@ -15,7 +15,7 @@ public class CheckReservationDAODateFieldBean {
     // 부스의 오픈 시간을 활용해서 몇일차인지 구함
     public Integer exec(NightBoothDAO nightBoothDAO) {
 
-        String openTime = nightBoothDAO.getOpenTime();
+        /*String openTime = nightBoothDAO.getOpenTime();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
         LocalTime open = LocalTime.parse(openTime, formatter).minusHours(2);
 
@@ -38,7 +38,31 @@ public class CheckReservationDAODateFieldBean {
             date = 3;
         } else {
             return null;
+        }*/
+
+        // 시작 시간
+        LocalDateTime start11 = LocalDateTime.of(LocalDate.of(2024, 9, 11), LocalTime.of(15, 0, 0));
+        LocalDateTime start12 = LocalDateTime.of(LocalDate.of(2024, 9, 12), LocalTime.of(15, 0, 0));
+        LocalDateTime start13 = LocalDateTime.of(LocalDate.of(2024, 9, 13), LocalTime.of(15, 0, 0));
+
+        // 종료 시간
+        LocalDateTime end11 = start11.plusHours(24);
+        LocalDateTime end12 = start12.plusHours(24);
+
+        // 서버 시간 고려 9시간 더해줌
+        LocalDateTime now = DateTimeUtils.nowZone();
+
+        // 시간에 따라 date 설정
+        int date = 0;
+
+        if(now.isAfter(start11) && now.isBefore(end11)) {
+            date = 1;
+        } else if(now.isAfter(start12) && now.isBefore(end12)) {
+            date = 2;
+        } else if(now.isAfter(start13)) {
+            date = 3;
         }
+
         return date;
     }
 }
