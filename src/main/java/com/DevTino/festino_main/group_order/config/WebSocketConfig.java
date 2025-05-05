0e2bda5b91480a53bf92a311a9c87cddab9bc988
yoosuhyeon <dbtngus20@naver.com>
2025-05-06 01:58:47 +0900

    feat : websocket connection logic

---
+++ src/main/java/com/DevTino/festino_main/group_order/config/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package com.DevTino.festino_main.group_order.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("*");
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 클라이언트로 메시지를 보낼 때 사용할 prefix
+        registry.enableSimpleBroker("/topic");
+
+        // 클라이언트에서 서버로 메시지를 보낼 때 사용할 prefix
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
