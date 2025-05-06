f2ab83db868ee8992fd169848e9f6e352fc305f3
yoosuhyeon <dbtngus20@naver.com>
2025-05-06 16:11:49 +0900

    feat : DTO for transferring data between redis client and server

---
+++ src/main/java/com/DevTino/festino_main/group_order/domain/DTO/OrderMessageDTO.java
@@ -0,0 +1,24 @@
+package com.DevTino.festino_main.group_order.domain.DTO;
+
+import com.DevTino.festino_main.message.domain.ENUM.MessageType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderMessageDTO {
+    MessageType type;
+    UUID boothId;
+    Integer tableNum;
+
+    // 메시지 타입에 따라 포함되는 정보 객체
+    InitInfo initInfo;       // 초기화 정보 (INIT 타입)
+    MenuInfo menuInfo;       // 메뉴 정보 (MENUUPDATE 타입)
+    MemberInfo memberInfo;   // 멤버 정보 (MEMBERUPDATE 타입)
+}
