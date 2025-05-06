51e661cacfea901d331ebe83c6bd7eba5774b1ca
yoosuhyeon <dbtngus20@naver.com>
2025-05-06 20:01:51 +0900

    feat : Timer-related settings

--- src/main/java/com/DevTino/festino_main/group_order/config/WebSocketConfig.java
+++ src/main/java/com/DevTino/festino_main/group_order/config/WebSocketConfig.java
@@ -1,7 +1,10 @@
 package com.DevTino.festino_main.group_order.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -23,5 +26,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
         // 클라이언트에서 서버로 메시지를 보낼 때 사용할 prefix
         registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(10);
+        scheduler.setThreadNamePrefix("ws-scheduler-");
+        scheduler.initialize();
+        return scheduler;
     }
 }
