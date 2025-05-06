c618a446a527088703f72d3d7753e50dbc81db87
yoosuhyeon <dbtngus20@naver.com>
2025-05-06 22:57:23 +0900

    fix : change repository inheritance

--- src/main/java/com/DevTino/festino_main/group_order/repository/OrderSessionRepository.java
+++ src/main/java/com/DevTino/festino_main/group_order/repository/OrderSessionRepository.java
@@ -1,7 +1,7 @@
 package com.DevTino.festino_main.group_order.repository;
 
 import com.DevTino.festino_main.group_order.domain.OrderSession;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface OrderSessionRepository extends JpaRepository<OrderSession, String> {
+public interface OrderSessionRepository extends CrudRepository<OrderSession, String> {
 }
