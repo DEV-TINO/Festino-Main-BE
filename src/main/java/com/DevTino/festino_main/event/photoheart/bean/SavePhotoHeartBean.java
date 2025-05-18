b15cff4ceb71e1fe54b568151cb55bb62980a1e5
niireymik <niireynik@tukorea.ac.kr>
2025-05-18 15:06:29 +0900

    fix : replace GetPhotoHeartDAOBean with CheckPhotoHeartDAOBean

--- src/main/java/com/DevTino/festino_main/event/photoheart/bean/SavePhotoHeartBean.java
+++ src/main/java/com/DevTino/festino_main/event/photoheart/bean/SavePhotoHeartBean.java
@@ -3,8 +3,8 @@ package com.DevTino.festino_main.event.photoheart.bean;
 import com.DevTino.festino_main.event.photo.bean.small.GetPhotoDAOBean;
 import com.DevTino.festino_main.event.photo.bean.small.SavePhotoDAOBean;
 import com.DevTino.festino_main.event.photo.domain.entity.PhotoDAO;
+import com.DevTino.festino_main.event.photoheart.bean.small.CheckPhotoHeartDAOBean;
 import com.DevTino.festino_main.event.photoheart.bean.small.CreatePhotoHeartDAOBean;
-import com.DevTino.festino_main.event.photoheart.bean.small.GetPhotoHeartDAOBean;
 import com.DevTino.festino_main.event.photoheart.bean.small.SavePhotoHeartDAOBean;
 import com.DevTino.festino_main.event.photoheart.domain.dto.RequestPhotoHeartSaveDTO;
 import com.DevTino.festino_main.event.photoheart.domain.entity.PhotoHeartDAO;
@@ -18,29 +18,29 @@ import java.util.UUID;
 @Component
 public class SavePhotoHeartBean {
 
-    GetPhotoHeartDAOBean getPhotoHeartDAOBean;
+    CheckPhotoHeartDAOBean checkPhotoHeartDAOBean;
     CreatePhotoHeartDAOBean createPhotoHeartDAOBean;
     GetPhotoDAOBean getPhotoDAOBean;
     SavePhotoHeartDAOBean savePhotoHeartDAOBean;
     SavePhotoDAOBean savePhotoDAOBean;
 
     @Autowired
-    public SavePhotoHeartBean(GetPhotoHeartDAOBean getPhotoHeartDAOBean, CreatePhotoHeartDAOBean createPhotoHeartDAOBean, GetPhotoDAOBean getPhotoDAOBean, SavePhotoHeartDAOBean savePhotoHeartDAOBean, SavePhotoDAOBean savePhotoDAOBean) {
-        this.getPhotoHeartDAOBean = getPhotoHeartDAOBean;
+    public SavePhotoHeartBean(CheckPhotoHeartDAOBean checkPhotoHeartDAOBean, CreatePhotoHeartDAOBean createPhotoHeartDAOBean, GetPhotoDAOBean getPhotoDAOBean, SavePhotoHeartDAOBean savePhotoHeartDAOBean, SavePhotoDAOBean savePhotoDAOBean) {
+        this.checkPhotoHeartDAOBean = checkPhotoHeartDAOBean;
         this.createPhotoHeartDAOBean = createPhotoHeartDAOBean;
         this.getPhotoDAOBean = getPhotoDAOBean;
         this.savePhotoHeartDAOBean = savePhotoHeartDAOBean;
         this.savePhotoDAOBean = savePhotoDAOBean;
     }
 
-    // 사진 게시물 사진 저장
+    // 사진 게시물 좋아요 저장
     public UUID exec(RequestPhotoHeartSaveDTO requestPhotoHeartSaveDTO) {
 
         // 이미 좋아요 되어 있으면 예외 발생
-        if (getPhotoHeartDAOBean.exec(requestPhotoHeartSaveDTO.getPhotoId(), requestPhotoHeartSaveDTO.getMainUserId()) != null)
+        if (checkPhotoHeartDAOBean.exec(requestPhotoHeartSaveDTO.getPhotoId(), requestPhotoHeartSaveDTO.getMainUserId()))
             throw new ServiceException(ExceptionEnum.ALREADY_PROCESSED);
 
-        // 사진 게시물 객체 생성
+        // 사진 게시물 좋아요 객체 생성
         PhotoHeartDAO photoHeartDAO = createPhotoHeartDAOBean.exec(requestPhotoHeartSaveDTO);
 
         // photoId 통해 원하는 photoDAO 객체 찾기
@@ -49,7 +49,7 @@ public class SavePhotoHeartBean {
         //찾은 photoDAO 객체 좋아요 추가
         photoDAO.setHeartCount(photoDAO.getHeartCount() + 1);
 
-        // 사진 게시물 좋아요 저장
+        // 사진 게시물  저장
         savePhotoHeartDAOBean.exec(photoHeartDAO);
 
         // 사진 게시물 저장
