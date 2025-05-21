5a434bb16de61707dd18513cc8332cb9a8174e6e
yoosuhyeon <dbtngus20@naver.com>
2025-05-22 08:59:57 +0900

    fix : modify to receive app message as init type when subscribed

--- src/main/java/com/DevTino/festino_main/group_order/domain/ENUM/AppMessageType.java
+++ src/main/java/com/DevTino/festino_main/group_order/domain/ENUM/AppMessageType.java
@@ -2,6 +2,7 @@ package com.DevTino.festino_main.group_order.domain.ENUM;
 
 public enum AppMessageType {
     // 클라이언트 -> 서버
+    INIT,           //
     MENUADD,        // 메뉴 수량 증가
     MENUSUB,        // 메뉴 수량 감소
     STARTORDER,     // 주문 시작
