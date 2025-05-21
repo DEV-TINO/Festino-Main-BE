51548639269949cd192ff0a6bdc74fc4578fbd8c
Lee <lsmin476@naver.com>
2025-05-21 17:47:50 +0900

    fix: exception1

--- src/main/java/com/DevTino/festino_main/exception/GlobalExceptionHandler.java
+++ src/main/java/com/DevTino/festino_main/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.DevTino.festino_main.exception;
 
 import com.DevTino.festino_main.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,7 +13,14 @@ import java.net.http.HttpResponse;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ServiceException.class)
-    public ResponseEntity<ApiResponse<Object>> handleServiceException(ServiceException ex) {
+    public ResponseEntity<ApiResponse<Object>> handleServiceException(ServiceException ex, HttpServletRequest request, HttpServletResponse response1) {
+        System.out.println("-------------------------------------------------GlobalError111111111111111111111111111111");
+
+        System.out.println("request.getPathInfo() = " + request.getPathInfo());
+        System.out.println("request.getRequestURI() = " + request.getRequestURI());
+        System.out.println("request.getRequestURL() = " + request.getRequestURL());
+        System.out.println("response1.getHeaderNames() = " + response1.getHeaderNames());
+
 
         // 성공 여부 실패로 설정, 예외 메시지 설정
         ApiResponse<Object> response = new ApiResponse<>(false, ex.getMessage(), null);
@@ -25,7 +33,16 @@ public class GlobalExceptionHandler {
 
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ApiResponse<Object>> handleRuntimeException(RuntimeException e, HttpServletResponse response1) {
+    public ResponseEntity<ApiResponse<Object>> handleRuntimeException(RuntimeException e, HttpServletRequest request, HttpServletResponse response1) {
+
+        System.out.println("-------------------------------------------------GlobalError");
+
+        System.out.println("request.getPathInfo() = " + request.getPathInfo());
+        System.out.println("request.getRequestURI() = " + request.getRequestURI());
+        System.out.println("request.getRequestURL() = " + request.getRequestURL());
+        System.out.println("response1.getHeaderNames() = " + response1.getHeaderNames());
+
+
         if(response1.isCommitted()){
             return null;
         }
